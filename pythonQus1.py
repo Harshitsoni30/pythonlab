a438a8be53467041fad2e7ecfd7f25364ef97908
Harshitsoni30 <91113306+Harshitsoni30@users.noreply.github.com>
2022-02-17 16:37:56 +0530

    Create pythonQus1.py

---
+++ pythonQus1.py
@@ -0,0 +1,23 @@
+x = 5
+print(type(x))
+
+x = str("Hello World")
+print(type(x))
+
+x = int(20)
+print(type(x))
+
+x = float(25.5)
+print(type(x))
+
+x = complex(1j)
+print(type(x))
+
+x = list(("apple", "orange"))
+print(type(x))
+
+x = dict(name="Pole", age=34)
+print(type(x))
+
+x = tuple(("apple", "orange"))
+print(type(x))
